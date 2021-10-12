0dd4c885a5b1f1f648187a71975d2fb22f5f94ab
AlexIvanov2021 <ivanovzpua1993@gmail.com>
2021-10-12 19:54:55 +0300

    version 3

--- app/src/main/java/com/example/lesson1/MainActivity.java
+++ app/src/main/java/com/example/lesson1/MainActivity.java
@@ -10,7 +10,7 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     Button red , green , blue;
-    ImageView imageView;
+    ImageView imageView , pic;
     TextView textView;
 
     @Override
@@ -23,12 +23,15 @@ public class MainActivity extends AppCompatActivity {
         green = findViewById(R.id.green);
         imageView = findViewById(R.id.image);
         textView = findViewById(R.id.text);
+        pic = findViewById(R.id.pic);
+
 
         red.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 imageView.setBackgroundResource(R.color.red);
                 textView.setText("Красный");
+                pic.setBackgroundResource(R.drawable.ic_baseline_add_ic_call_24);
 
             }
         });
@@ -38,6 +41,8 @@ public class MainActivity extends AppCompatActivity {
           public void onClick(View view) {
               imageView.setBackgroundResource(R.color.blue);
               textView.setText("Синий");
+              pic.setBackgroundResource(R.drawable.ic_baseline_baby_changing_station_24);
+
 
           }
       });
@@ -46,6 +51,8 @@ public class MainActivity extends AppCompatActivity {
     public void setGreenColor(View view){
         imageView.setBackgroundResource(R.color.teal_200);
         textView.setText("Зелёный");
+        pic.setBackgroundResource(R.drawable.ic_baseline_bedtime_24);
+
 
     }
 
